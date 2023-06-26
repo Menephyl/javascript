55dec332f53b6c41c2753c71c1dd1a1c4451351c
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2023-06-26 13:28:09 -0300

    update intro_js
    ainda aprendendo o básico de js

---
+++ trybe_introducao/arrays_aula.js
@@ -0,0 +1,43 @@
+let itens = ['escova de dentes', 'boneco do jaspion','toalhas']
+let arr =[2, false, 'boneco',itens] 
+arr
+itens[0]
+let coisasParaViajar = 'escova de dentes,boneco do jaspion, toalhas' 
+undefined
+coisasParaViajar
+'escova de dentes,boneco do jaspion, toalhas'
+let itens =['escova de dentes','boneco do jaspion','toalhas']
+undefined
+let arr = [2,false,'boneco', itens]
+undefined
+arr
+(4) [2, false, 'boneco', Array(3)]
+itens[0]
+'escova de dentes'
+const item ='boneco do jaspion'
+undefined
+const primeiraLetra =item[1]
+undefined
+primeiraLetra
+'o'
+itens
+(3) ['escova de dentes', 'boneco do jaspion', 'toalhas']
+itens[3] = 'roupas de frio'
+'roupas de frio'
+itens
+(4) ['escova de dentes', 'boneco do jaspion', 'toalhas', 'roupas de frio']
+itens.length
+4
+itens[itens.lenght] = 'roupas de calor'
+'roupas de calor'
+const coberturas=['C','M','D']
+undefined
+console.log(sabores.length)
+VM2590:1 5
+undefined
+let lista =[1,2]
+undefined
+let inverter = true
+undefined
+let listaInvertida=[]
+undefined
